30affa020a0cf971d99dc634f9f93f0841aa0a1a
Pawan Joshi <95375077+pwnjoshi@users.noreply.github.com>
2025-10-12 02:35:34 +0000

    DAY 2 Done

---
+++ 01_basics/test.js
@@ -0,0 +1,23 @@
+const accountId = 10
+let accountEmail= "chai@peelo.com"
+var accountPass = "1212"
+accountCity = "JAIPUR" // you may use variable withou defining but should not use ( not a good practice )
+
+// accountId = 2 // NOT ALLOWED ( const hai na )
+
+
+let accountState 
+
+
+accountEmail = "uskichai@matpiyo@gmail.com"
+accountPass = "454545"
+accountCity = " Bengaluru "
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPass,accountCity, accountState])
+
+
+/* 
+Prefer not to use var because of the issue in  block scope and functional scope
+*/
