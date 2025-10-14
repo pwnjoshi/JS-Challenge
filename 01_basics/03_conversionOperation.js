0cc76a999d5701cbda4196ad1da8187a63916979
Pawan Joshi <95375077+pwnjoshi@users.noreply.github.com>
2025-10-14 02:34:12 +0000

    Day 5 of JS Challenge

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,53 @@
+// DAY 5 Of JS Challenge
+
+// Conversion Operation
+
+// 1. Implicit Conversion
+// 2. Explicit Conversion
+
+// Implicit Conversion
+
+// console.log("5" + 3); // "53" => String
+// console.log("5" - 3); // 2 => Number
+// console.log("5" * "3"); // 15 => Number
+// console.log("10" / "2"); // 5 => Number
+// console.log("5" + true); // "5true" => String
+// console.log("5" - true); // 4 => Number
+// console.log(false + true); // 1 => Number
+// console.log(2 + null); // 2 => Number
+// console.log(2 + undefined); // NaN => Number
+
+// Explicit Conversion - kal
+
+// from yt
+
+
+let score = "Meri Chai"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "Meri Chai"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Meri Chai" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
