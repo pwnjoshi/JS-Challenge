ce6b5b5787c3937562c6242b828d54e151ee6443
Pawan Joshi <95375077+pwnjoshi@users.noreply.github.com>
2025-10-15 20:11:52 +0000

    DAY 6 Done

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,26 @@
+// DAY 6
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+// We mostly avoid these type of code in order to maintain cleanliness of code
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
